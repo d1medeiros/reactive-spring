db25d754db3557ef0310a34796cc4f3d9fec6a5f
aian <djbmbb@hotmail.com>
2018-11-02 12:23:06 -0300

    inicio

---
+++ src/main/java/com/example/demoreactiveone/Event.java
@@ -0,0 +1,29 @@
+package com.example.demoreactiveone;
+
+import java.util.Date;
+
+public class Event {
+    private  long id;
+    private  Date when;
+
+    public Event(long id, Date when) {
+        this.id = id;
+        this.when = when;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getWhen() {
+        return when;
+    }
+
+    public void setWhen(Date when) {
+        this.when = when;
+    }
+}
